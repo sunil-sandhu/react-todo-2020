ca6d9e8c857e8e04bf67b033e9e79b6bbb2f53c5
Sunil Sandhu <sunil.sandhu87@gmail.com>
2020-06-13 18:18:30 +0200

    updated ui and added fn for displaying user errors

---
+++ src/components/ToDo.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Logo from "../assets/logo.png";
+import ToDoItem from "./ToDoItem";
+import "./ToDo.css";
+
+const ToDo = () => {
+  const [list, setList] = useState([
+    { id: 1, text: "clean the house" },
+    { id: 2, text: "buy milk" },
+  ]);
+  const [toDo, setToDo] = useState("");
+  const [showError, setShowError] = useState(false);
+
+  const generateId = () => {
+    if (list && list.length > 1) {
+      return Math.max(...list.map((t) => t.id)) + 1;
+    } else {
+      return 1;
+    }
+  };
+
+  const displayError = () => {
+    setShowError(true);
+    const clearTimer = setTimeout(() => setShowError(false), 3000);
+    return () => clearTimeout(clearTimer);
+  };
+  const createNewToDoItem = () => {
+    //validate todo
+    if (!toDo) {
+      displayError();
+      return;
+    }
+    const newId = generateId();
+    const newToDo = { id: newId, text: toDo };
+    setList([...list, newToDo]);
+    setToDo("");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      createNewToDoItem();
+    }
+  };
+
+  const handleInput = (e) => {
+    setToDo(e.target.value);
+  };
+
+  const deleteItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="ToDo">
+      <img className="Logo" src={Logo} alt="React logo" />
+      <h1 className="ToDo-Header">React To Do</h1>
+      <div className="ToDo-Container">
+        <div className="ToDo-Content">
+          {list.map((item) => {
+            return <ToDoItem key={item.id} item={item} deleteItem={deleteItem} />;
+          })}
+        </div>
+
+        <div className="ToDoInput">
+          <input type="text" value={toDo} onChange={handleInput} onKeyPress={handleKeyPress} />
+          <button className="ToDo-Add" onClick={createNewToDoItem}>
+            +
+          </button>
+        </div>
+        <div className="ToDo-ErrorContainer">{showError && <p>Please enter a todo!</p>}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ToDo;
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ToDoItem.css";
+
+const ToDoItem = (props) => {
+  const { item, deleteItem } = props;
+
+  return (
+    <div className="ToDoItem">
+      <p className="ToDoItem-Text">{item.text}</p>
+      <button className="ToDoItem-Delete" onClick={() => deleteItem(item.id)}>
+        -
+      </button>
+    </div>
+  );
+};
+
+export default ToDoItem;
